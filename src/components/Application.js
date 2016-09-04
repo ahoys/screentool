3f1cf18bab0bac71425c4246c49d8bf14eda7a89
Ari Höysniemi <ari.hoysniemi@gmail.com>
2016-09-04 18:17:27 +0300

    Refactored to more flux-like approach.

---
+++ src/components/Application.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Application.css';
+
+class Application extends Component {
+    render() {
+        return (
+            <h1>Daym, it works.</h1>
+        );
+    }
+}
+
+export default Application;
