cb7dbfc8de620055a81eb1c056a2ce5be46d2dc6
Ari Höysniemi <ari.hoysniemi@gmail.com>
2016-09-05 23:21:44 +0300

    Read resolution for aspect screens.

--- src/components/Specification/AspectDisplayFrame.js
+++ src/components/Specification/AspectDisplayFrame.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class AspectDisplayFrame extends Component {
-    render() {
+const AspectDisplayFrame = React.createClass({
+
+    render: function () {
+
         return (
             <div className="AspectDisplayFrame">
                 <p>#AspectDisplayFrame</p>
+                <p>{this.props.x0} x {this.props.y0}</p>
+                <p>{this.props.x1} x {this.props.y1}</p>
             </div>
         );
     }
-}
+});
 
 export default AspectDisplayFrame;
