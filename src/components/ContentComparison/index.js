dd6a1b02a8659af6adf7fbd24f88da7ff3455c86
Ari Höysniemi <ari.hoysniemi@gmail.com>
2016-09-07 22:14:55 +0300

    Removed additional size comparison.

--- src/components/ContentComparison/index.js
+++ src/components/ContentComparison/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './ContentComparison.css';
 
+import Table from './parts/Table';
+
 const ContentComparison = React.createClass({
 
     render: function () {
 
         return (
             <div className="ContentComparison">
-
+                <Table screens={this.props.screens}/>
             </div>
         );
     }
