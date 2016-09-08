703b3a3004ba160b13448834a79b6e6066b59af4
Ari Höysniemi <ari.hoysniemi@gmail.com>
2016-09-08 11:43:10 +0300

    Table poundering.

--- src/components/ContentComparison/parts/Table.js
+++ src/components/ContentComparison/parts/Table.js
@@ -5,9 +5,23 @@ const Table = React.createClass({
 
     render: function () {
 
+        const highlight = {
+            aspectRatio: 0
+        };
+
+        const dataHTML = Object.keys(this.props.screens).map(function (key) {
+
+            const screen = this.props.screens[key];
+            return(
+                <div class="screen">
+                    test
+                </div>
+            );
+        }.bind(this));
+
         return (
             <div className="Table">
-
+                {dataHTML}
             </div>
         );
     }
