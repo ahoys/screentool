58752caeea7a61ce51d786e2110f30241e458a40
Ari Höysniemi <ari.hoysniemi@gmail.com>
2016-09-10 12:26:29 +0300

    Initial Content component.

---
+++ src/components/Content/parts/Comparison.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Comparison.css';
+
+const Comparison = React.createClass({
+
+    render: function () {
+        return (
+            <div className="Comparison">
+                #Comparison
+            </div>
+        );
+    }
+});
+
+export default Comparison;
---
+++ src/components/Content/parts/Control.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Control.css';
+
+const Control = React.createClass({
+
+    render: function () {
+        return (
+            <div className="Control">
+                #Control
+            </div>
+        );
+    }
+});
+
+export default Control;
